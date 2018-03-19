f149c7ca97dd8f233a2e21a0b33653b0901d9e38
YuHanm <1138941694@qq.com>
2018-03-19 19:02:48 +0800

    增加csdn登录操作

--- Selenium/csdn_login.py
+++ Selenium/csdn_login.py
@@ -11,4 +11,21 @@ driver.find_element_by_css_selector('#username').send_keys('1138941694@qq.com')
 driver.find_element_by_css_selector('.pass-word').send_keys('521125')
 driver.find_element_by_class_name('logging').click()
 time.sleep(5)
+#鼠标悬停
+blogs = driver.find_element_by_css_selector('.loginCenter')
+ActionChains(driver).move_to_element(blogs).perform()
+time.sleep(4)
+#获取当前窗口句柄
+
+
+#点击博客
 driver.find_element_by_css_selector('.toolbar_to_feed').click()
+
+#切换窗口
+h = driver.current_window_handle
+print(h)
+time.sleep(10)
+all_h = driver.window_handles
+print(all_h)
+driver.switch_to.window(all_h[0])
+print(driver.title)
